1a33c0570b38ea0aa1c215dca4a0ee8216d34beb
Ayush_Kumar <akayushkumarak001@gmail.com>
2026-04-29 15:26:31 +0530

    types and type narrowing

--- src/TypeNarrowing.ts
+++ src/TypeNarrowing.ts
@@ -4,3 +4,25 @@ function getChai(kind: string|number){
     }
     return `Chai order: ${kind}`
 }
+
+
+type chaiOrder = {
+    type:String,
+    sugar:number
+}
+
+function isChaiOrder(obj:any):obj is chaiOrder{
+    return(
+        typeof obj === "object"&&
+        obj !== null &&
+        typeof obj.type === "string"&&
+        typeof obj.sugar === "number"
+    )
+}
+
+function serveOrder(item:chaiOrder|string){
+    if(isChaiOrder(item)){
+        return `Serving chai`
+    }
+    return `not serving chai`
+}
---
+++ src/moreTypes.ts
@@ -0,0 +1,7 @@
+//forceful insertion
+
+let resp:any="43";
+let numericLen:number =(resp as string).length;
+
+//type assertion
+const inputElement = document.getElementById("username") as HTMLInputElement;
